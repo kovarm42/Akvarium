ee768b0acb4e3982786fb13cb03ea2a766d525cd
kovarm42 <97522309+kovarm42@users.noreply.github.com>
2022-01-27 22:44:33 +0100

    Zmeny 27012022/2244

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace Akvarium
 {
@@ -14,11 +15,39 @@ namespace Akvarium
         {
 
             string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Zverimex_A;Integrated Security=True";
-            using (SqlConnection pripojeni = new SqlConnection(connectionString))
+            using (SqlConnection spojeni = new SqlConnection(connectionString))
+
             {
-                pripojeni.Open();
-                Console.WriteLine("Aplikace se úspěšně připojila k databázi.");
+                spojeni.Open();
+
+                Console.WriteLine("Dotaz_1");
+
+                string dotaz = "SELECT [Akvarium_nazev],[Zakaznik].[Jmeno],[Zakaznik].[Prijmeni] FROM[Akvarium] INNER JOIN[Zakaznik] ON[Akvarium].[Id_zakaznik] = [Zakaznik].[Id_Zakaznik] ORDER BY[Akvarium_nazev];";
+                using (SqlDataAdapter adapter = new SqlDataAdapter(dotaz, spojeni))
+                using (DataSet vysledky = new DataSet())
+                {
+                    adapter.Fill(vysledky);
+
+                    foreach (DataRow radek in vysledky.Tables[0].Rows)
+                    {
+                        Console.WriteLine("Jmeno: " + radek[0] + ", Prijmeni: " + radek[1] + ", anglicky: " + radek[2]);
+                    }
+
+                    
+                }
+
+                spojeni.Close();
             }
+
+
+
+
+
+
+
+
+            
+
             Console.ReadKey();
 
 
--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,4 @@
-﻿
+﻿SELECT [Akvarium_nazev],[Zakaznik].[Jmeno],[Zakaznik].[Prijmeni]
+FROM [Akvarium]  
+INNER JOIN [Zakaznik] ON [Akvarium].[Id_zakaznik] = [Zakaznik].[Id_Zakaznik]
+ORDER BY [Akvarium_nazev];
