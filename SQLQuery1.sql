64b8b74b2e5ef6b8f6d11d6575af6af1ecbea507
kovarm42 <97522309+kovarm42@users.noreply.github.com>
2022-01-28 13:17:53 +0100

    zmena28012022/1317

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,15 +1,18 @@
 ﻿/* SELECT [Akvarium_nazev],[Zakaznik].[Jmeno],[Zakaznik].[Prijmeni]
 FROM [Akvarium]  
 INNER JOIN [Zakaznik] ON [Akvarium].[Id_zakaznik] = [Zakaznik].[Id_Zakaznik]
-ORDER BY [Akvarium_nazev];*/
+ORDER BY [Akvarium_nazev];
 
 
 
 SELECT [Ryba_nazev],[Akvarium_nazev],[Datum_pridani],[Datum_odebrani]
 FROM [Obsah_akvaria]  
 INNER JOIN [Ryba] ON [Obsah_akvaria].[Id_Ryba] = [Ryba].[Id_Ryba]
+INNER JOIN [Akvarium] ON [Obsah_akvaria].[Id_akvarium] = [Akvarium].[Id_Akvarium];*/
+
+
+SELECT [Ryba_nazev],[Akvarium_nazev],[Datum_pridani],[Datum_odebrani]
+FROM [Obsah_akvaria]  
+INNER JOIN [Ryba] ON [Obsah_akvaria].[Id_Ryba] = [Ryba].[Id_Ryba]
 INNER JOIN [Akvarium] ON [Obsah_akvaria].[Id_akvarium] = [Akvarium].[Id_Akvarium];
-
-
-
 
